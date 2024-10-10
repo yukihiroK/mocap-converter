2672f21d72b282be8431360ccc95bad2b3ff7018
yukihiroK <57009247+yukihiroK@users.noreply.github.com>
2024-10-10 11:59:02 +0900

    feat: kinematic tree class

---
+++ src/bvh_converter/kinematic_tree.py
@@ -0,0 +1,109 @@
+from typing import Optional, List, TypedDict
+
+from bvh_converter.node import Node
+
+
+class KinematicTree:
+    """A kinematic tree representing a hierarchy of joints and end effectors.
+
+    The tree has a root node, which is the top-level joint in the hierarchy.
+    Each node in the tree has a name, a parent, and a list of children.
+    The parent and children are other nodes in the tree.
+
+    Attributes:
+        root: The root node of the tree.
+        nodes: A dictionary mapping node names to nodes.
+
+    Methods:
+        get_node: Get a node by name.
+        add_node: Add a node to the tree.
+        remove_node: Remove a node from the tree.
+
+    """
+
+    class NoRootError(Exception):
+        """Raised when the kinematic tree has no root node"""
+
+        pass
+
+    class NotConnectedError(Exception):
+        """Raised when a node is not connected to the tree or the tree has multiple roots"""
+
+        pass
+
+    def __init__(self, nodes: List[Node]):
+        self.__root: Optional[Node] = None
+        self.__nodes: dict[str, Node] = {}
+
+        self.add_nodes(nodes)
+
+    @classmethod
+    def from_params(cls, nodes: List[Node.NodeParams]) -> "KinematicTree":
+        root_nodes = [node for node in nodes if not node.get("parent")]
+        if len(root_nodes) < 1:
+            raise cls.NoRootError
+        if len(root_nodes) > 1:
+            raise cls.NotConnectedError
+        root_params = root_nodes[0]
+
+        result = []
+
+        def build_tree(node_params: Node.NodeParams) -> Node:
+            node = Node(**{**node_params, "parent": None})
+            result.append(node)
+            for child_params in nodes:
+                if child_params.get("parent") == node_params["name"]:
+                    child_node = build_tree(child_params)
+                    node._add_child(child_node)
+            return node
+
+        build_tree(root_params)
+
+        return cls(result)
+
+    def get_node(self, name: str) -> Optional[Node]:
+        return self.__nodes.get(name)
+
+    def add_node(self, node: Node):
+        if node.name in self.__nodes:
+            return
+
+        self.__nodes[node.name] = node
+
+        if node.parent is None:
+            if self.__root is not None:
+                raise self.NotConnectedError
+            self.__root = node
+        elif node.parent.name not in self.__nodes:
+            self.add_node(node.parent)
+
+        if self.__root is None:  # called after all nodes are added recursively
+            raise self.NoRootError
+
+    def add_nodes(self, nodes: List[Node]):
+        for node in nodes:
+            self.add_node(node)
+
+    def __remove_node(self, node: Node):
+        if node.parent is not None:
+            node.parent._remove_child(node)
+        for child in node.children:
+            self.__remove_node(child)
+        del self.__nodes[node.name]
+
+    def remove_node(self, name: str):
+        node = self.__nodes[name]
+        if node == self.__root:
+            self.__root = None
+        self.__remove_node(node)
+
+    @property
+    def root(self) -> Optional[Node]:
+        return self.__root
+
+    @property
+    def nodes(self) -> dict[str, Node]:
+        return self.__nodes.copy()
+
+    def __repr__(self):
+        return f"KinematicTree(root={self.__root}, nodes={self.__nodes})"
---
+++ tests/test_kinematic_tree.py
@@ -0,0 +1,181 @@
+import pytest
+
+from bvh_converter.node import Node
+from bvh_converter.kinematic_tree import KinematicTree
+
+
+def test_kinematic_tree_remove_node():
+    root = Node("root")
+    child1 = Node("child1", parent=root)
+    child2 = Node("child2", parent=root)
+    grandchild1 = Node("grandchild1", parent=child1)
+    grandchild2 = Node("grandchild2", parent=child1)
+
+    tree = KinematicTree([root, child1, child2, grandchild1, grandchild2])
+
+    assert tree.get_node("root") == root
+    assert tree.get_node("child1") == child1
+    assert tree.get_node("child2") == child2
+    assert tree.get_node("grandchild1") == grandchild1
+    assert tree.get_node("grandchild2") == grandchild2
+
+    tree.remove_node("child1")
+
+    assert tree.get_node("root") == root
+    assert tree.get_node("child1") is None
+    assert tree.get_node("child2") == child2
+    assert tree.get_node("grandchild1") is None
+    assert tree.get_node("grandchild2") is None
+
+
+def test_kinematic_tree_from_dict_remove_node():
+
+    nodes: list[Node.NodeParams] = [
+        {"name": "root", "parent": None},
+        {"name": "child1", "parent": "root"},
+        {"name": "child2", "parent": "root"},
+        {"name": "grandchild1", "parent": "child1"},
+        {"name": "grandchild2", "parent": "child1"},
+    ]
+
+    tree = KinematicTree.from_params(nodes)
+
+    root = tree.get_node("root")
+    child1 = tree.get_node("child1")
+    child2 = tree.get_node("child2")
+    grandchild1 = tree.get_node("grandchild1")
+    grandchild2 = tree.get_node("grandchild2")
+
+    assert root is not None
+    assert child1 is not None
+    assert child2 is not None
+    assert grandchild1 is not None
+    assert grandchild2 is not None
+
+    assert root.parent is None
+    assert child1.parent == root
+    assert child2.parent == root
+    assert grandchild1.parent == child1
+    assert grandchild2.parent == child1
+
+    tree.remove_node("child1")
+
+    assert tree.get_node("root") == root
+    assert tree.get_node("child1") is None
+    assert tree.get_node("child2") == child2
+    assert tree.get_node("grandchild1") is None
+    assert tree.get_node("grandchild2") is None
+
+
+def test_kinematic_tree_from_dict_remove_root():
+
+    nodes: list[Node.NodeParams] = [
+        {"name": "root", "parent": None},
+        {"name": "child1", "parent": "root"},
+        {"name": "child2", "parent": "root"},
+        {"name": "grandchild1", "parent": "child1"},
+        {"name": "grandchild2", "parent": "child1"},
+    ]
+
+    tree = KinematicTree.from_params(nodes)
+
+    root = tree.get_node("root")
+    child1 = tree.get_node("child1")
+    child2 = tree.get_node("child2")
+    grandchild1 = tree.get_node("grandchild1")
+    grandchild2 = tree.get_node("grandchild2")
+
+    assert root is not None
+    assert child1 is not None
+    assert child2 is not None
+    assert grandchild1 is not None
+    assert grandchild2 is not None
+
+    assert root.parent is None
+    assert child1.parent == root
+    assert child2.parent == root
+    assert grandchild1.parent == child1
+    assert grandchild2.parent == child1
+
+    tree.remove_node("root")
+
+    assert tree.get_node("root") is None
+    assert tree.get_node("child1") is None
+    assert tree.get_node("child2") is None
+    assert tree.get_node("grandchild1") is None
+    assert tree.get_node("grandchild2") is None
+
+
+def test_kinematic_tree_add_node():
+    tree = KinematicTree([])
+
+    assert tree.root is None
+    assert tree.nodes == {}
+
+    root = Node("root")
+    tree.add_node(root)
+
+    assert tree.root == root
+    assert tree.nodes == {"root": root}
+
+    child1 = Node("child1", parent=root)
+    tree.add_node(child1)
+
+    assert tree.root == root
+    assert tree.nodes == {"root": root, "child1": child1}
+
+    child2 = Node("child2", parent=root)
+    tree.add_node(child2)
+
+    assert tree.root == root
+    assert tree.nodes == {"root": root, "child1": child1, "child2": child2}
+
+    grandchild1 = Node("grandchild1", parent=child1)
+    tree.add_node(grandchild1)
+
+    assert tree.root == root
+    assert tree.nodes == {"root": root, "child1": child1, "child2": child2, "grandchild1": grandchild1}
+
+    grandchild2 = Node("grandchild2", parent=child1)
+    tree.add_node(grandchild2)
+
+    assert tree.root == root
+    assert tree.nodes == {
+        "root": root,
+        "child1": child1,
+        "child2": child2,
+        "grandchild1": grandchild1,
+        "grandchild2": grandchild2,
+    }
+
+
+def test_kinematic_tree_validate_no_root():
+    tree = KinematicTree([])
+
+    assert tree.root is None
+
+
+def test_kinematic_tree_validate_multiple_roots():
+    root1 = Node("root1")
+    root2 = Node("root2")
+
+    with pytest.raises(KinematicTree.NotConnectedError):
+        KinematicTree([root1, root2])
+
+
+def test_kinematic_tree_validate_cycle():
+    root = Node("root")
+    child = Node("child", parent=root)
+    grandchild = Node("grandchild", parent=child)
+    grandchild._add_child(root)
+
+    with pytest.raises(KinematicTree.NoRootError):
+        KinematicTree([root, child, grandchild])
+
+
+def test_kinematic_tree_validate_node_not_connected():
+    root = Node("root")
+    child = Node("child", parent=Node("parent"))
+
+    with pytest.raises(KinematicTree.NotConnectedError):
+        KinematicTree([root, child])
