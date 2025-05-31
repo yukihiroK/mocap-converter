849898edc6a9ab935bfde3ad0e2884db4c8f9488
yukihiroK <57009247+yukihiroK@users.noreply.github.com>
2025-06-01 01:13:41 +0900

    refactor: rename position_data and rotation_data to positions and rotations in MotionData class

--- src/bvh_converter/motion_data.py
+++ src/bvh_converter/motion_data.py
@@ -10,8 +10,8 @@ class MotionData:
     def __init__(
         self,
         kinematic_tree: KinematicTree,
-        position_data: Optional[Dict[str, np.ndarray]] = None,
-        rotation_data: Optional[Dict[str, np.ndarray]] = None,
+        positions: Optional[Dict[str, np.ndarray]] = None,
+        rotations: Optional[Dict[str, np.ndarray]] = None,
         frame_time: float = 1 / 30,
     ):
         """
@@ -19,15 +19,15 @@ class MotionData:
 
         Attributes:
             kinematic_tree (KinematicTree): The kinematic tree that the motion data is associated with.
-            position_data (Optional[Dict[str, np.ndarray]]): A dictionary of position data for each node in the kinematic tree. The keys are node names and the values are numpy arrays of shape (N, 3), where N is the number of frames.
-            rotation_data (Optional[Dict[str, np.ndarray]]): A dictionary of rotation data for each node in the kinematic tree. The keys are node names and the values are numpy arrays of shape (N, 4), where N is the number of frames. The order of the quaternion elements is (x, y, z, w).
+            positions (Optional[Dict[str, np.ndarray]]): A dictionary of position data for each node in the kinematic tree. The keys are node names and the values are numpy arrays of shape (N, 3), where N is the number of frames.
+            rotations (Optional[Dict[str, np.ndarray]]): A dictionary of rotation data for each node in the kinematic tree. The keys are node names and the values are numpy arrays of shape (N, 4), where N is the number of frames. The order of the quaternion elements is (x, y, z, w).
             frame_time (float): The time between frames in seconds.
         """
 
         frame_count = 0
-        if position_data:
-            frame_count = len(list(position_data.values())[0])
-            for name, position in position_data.items():
+        if positions:
+            frame_count = len(list(positions.values())[0])
+            for name, position in positions.items():
                 if position.shape[0] != frame_count:
                     raise ValueError(
                         f"Position data for '{name}' must have the same number of frames as other data: {position.shape[0]} frames, expected {frame_count}"
@@ -35,11 +35,11 @@ class MotionData:
                 if position.shape[1] != 3:
                     raise ValueError(f"Position data for '{name}' must have shape (N, 3): {position.shape}")
 
-        if rotation_data:
+        if rotations:
             if frame_count == 0:
-                frame_count = len(list(rotation_data.values())[0])
+                frame_count = len(list(rotations.values())[0])
 
-            for name, rotation in rotation_data.items():
+            for name, rotation in rotations.items():
                 if rotation.shape[0] != frame_count:
                     raise ValueError(
                         f"Rotation data for '{name}' must have the same number of frames as other data: {rotation.shape[0]} frames, expected {frame_count}"
@@ -48,8 +48,8 @@ class MotionData:
                     raise ValueError(f"Rotation data for '{name}' must have shape (N, 4): {rotation.shape}")
 
         self.kinematic_tree = kinematic_tree
-        self.__position_data = position_data if position_data else {}
-        self.__rotation_data = rotation_data if rotation_data else {}
+        self.__node_positions = positions if positions else {}
+        self.__node_rotations = rotations if rotations else {}
         self.__frame_count = frame_count
         self.frame_time = frame_time
 
@@ -57,19 +57,17 @@ class MotionData:
     def frame_count(self) -> int:
         return self.__frame_count
 
-    # @property
-    # def position_data(self) -> Dict[str, np.ndarray]:
-    #     return self.__position_data.copy()
+    def has_positions(self, name: str) -> bool:
+        return name in self.__node_positions
 
-    # @property
-    # def rotation_data(self) -> Dict[str, np.ndarray]:
-    #     return self.__rotation_data.copy()
+    def has_rotations(self, name: str) -> bool:
+        return name in self.__node_rotations
 
     def get_positions(self, name: str) -> np.ndarray:
-        return self.__position_data[name]
+        return self.__node_positions[name].copy()
 
     def get_rotations(self, name: str) -> np.ndarray:
-        return self.__rotation_data[name]
+        return self.__node_rotations[name].copy()
 
     def set_positions(self, name: str, position: np.ndarray):
         if position.shape[0] != self.__frame_count:
@@ -79,7 +77,7 @@ class MotionData:
         if position.shape[1] != 3:
             raise ValueError(f"Position data for '{name}' must have shape (N, 3): {position.shape}")
 
-        self.__position_data[name] = position
+        self.__node_positions[name] = position.copy()
 
     def set_rotations(self, name: str, rotation: np.ndarray):
         if rotation.shape[0] != self.__frame_count:
@@ -89,4 +87,4 @@ class MotionData:
         if rotation.shape[1] != 4:
             raise ValueError(f"Rotation data for '{name}' must have shape (N, 4): {rotation.shape}")
 
-        self.__rotation_data[name] = rotation
+        self.__node_rotations[name] = rotation.copy()
