fdf101f493294becc9ee7efafdda8392717e9bb3
yukihiroK <57009247+yukihiroK@users.noreply.github.com>
2025-09-14 02:27:52 +0900

    chore: update `pyproject.toml`

--- src/mocap_converter/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present yukihiroK <57009247+yukihiroK@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present yukihiroK <57009247+yukihiroK@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
